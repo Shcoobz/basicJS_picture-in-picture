6adf28ee480cd4cd423b993f8b6ac4e97d569a3c
Shcoobz <a.boeckmann23@gmail.com>
2023-06-02 11:18:54 +0200

    removed comments

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// prompt select media stream, pass to video element, play
 async function selectMediaStream() {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-
     videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
-      //videoElement.play();
       videoElement.addEventListener('play', handlePlay);
     };
-  } catch (error) {
-    // catch error
-    console.log('whoops, error here:', error);
-  }
+  } catch (error) {}
 }
 
 function handlePlay() {
-  videoElement.requestPictureInPicture(); // Automatically enter PiP mode when the video starts playing
-  videoElement.removeEventListener('play', handlePlay); // Remove the event listener once it's triggered
+  videoElement.requestPictureInPicture();
+  videoElement.removeEventListener('play', handlePlay);
 }
 
 button.addEventListener('click', async () => {
-  // disable button
   button.disabled = true;
-  // start play
   await videoElement.play();
-  await videoElement.requestPictureInPicture();
-  // reset button
+  //await videoElement.requestPictureInPicture();
   button.disabled = false;
-  // start pip
 });
 
 selectMediaStream();
